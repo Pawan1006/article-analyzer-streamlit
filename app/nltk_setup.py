8d7a50e95ab77c27bda58e93a4f3e54d1c8e11d0
Pawan Solanki <pawansolankipachore@hotmail.com>
2025-06-25 00:05:37 +0530

    add punkt_tab

--- app/nltk_setup.py
+++ app/nltk_setup.py
@@ -1,7 +1,7 @@
 import nltk
 
 def ensure_nltk_resources():
-    for resource in ["punkt", "stopwords", "vader_lexicon", "wordnet", "omw-1.4"]:
+    for resource in ["punkt", "stopwords", "vader_lexicon", "wordnet", "omw-1.4", "punkt_tab"]:
         try:
             nltk.data.find(resource)
         except LookupError:
