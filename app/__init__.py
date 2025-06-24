dabd68ed079e1ec1763368ec754e421e5a66fdcf
Pawan Solanki <pawansolankipachore@hotmail.com>
2025-06-24 16:41:02 +0530

    Resolved NLTK dependencies and moved to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,7 @@ import nltk
 
 nltk.download("punkt", quiet=True)
 nltk.download("stopwords", quiet=True)
-nltk.download("vader_lexicon", quiet=True)
+nltk.download("vader_lexicon", quiet=True) 
+nltk.download('wordnet', quiet=True)
+nltk.download('omw-1.4', quiet=True)
+nltk.download('punkt_tab', quiet=True)
